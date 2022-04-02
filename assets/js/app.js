236b888b0044d158cb59868a5c01c4801d6034dc
ravi7483 <somwadvaibhav@gmail.com>
2022-04-02 09:50:47 +0530

    hoisting
    main

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+console.log("app js file");
+
+console.log(x); // undefined
+
+var x = 10;
+console.log(x); // 10 
+
+console.log(y); // undefined
+var y;
+
+var z;
+console.log(z); // undefined
+
+
+// hoisting :- shifting all declration at the top of scope (page),
+             
+ //           :- all the declration in our page (scope) are get shifted at the top of scope this is called hoisting
+
+
+ 
+ console.log(p); // undefined
+
+ var p = 20;
+
+ console.log(p); // 20 
